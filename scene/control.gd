8efb2f1d08a2b296b0237993037d3a8891edc43c
xiaoweiI <41134493+xiaoweiI@users.noreply.github.com>
2025-02-20 21:33:35 -0500

    change

--- scene/control.gd
+++ scene/control.gd
@@ -13,11 +13,12 @@ func generate_grid():
 		grid.append([])
 		for col in range(GRID_SIZE):
 			var tile = TILE_SCENE.instantiate()
-			tile.set_type(randi() % 5)  
+			var random_type = randi() % 5
+			tile.set_type(random_type)
 			tile.connect("pressed", Callable(self, "_on_tile_pressed").bind(row, col))
 			grid[row].append(tile)
 			$GridContainer.add_child(tile)
-
+			
 func _on_tile_pressed(row, col):
 	print("Clicked tile at: ", row, col)
 	
